8bf1c33b9b1ddcc0c8318ae22e9e2feb366ac2ed
Metin Akın <aknmetin@gmail.com>
2023-08-05 20:18:54 +0300

    Axios fix

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -2,13 +2,13 @@
 import Link from "next/link";
 import React from "react";
 import { useRouter } from "next/navigation";
-import { axios } from "axios";
+import axios from "axios";
 
 
 export default function SignupPage() {
 
     const [user, setUser] = React.useState({
-        
+
         username: "",
     })
     const onSignup = async () => {
@@ -20,8 +20,8 @@ export default function SignupPage() {
         <div className="flex flex-col items-center justify-center min-h-screen py-2">
             <h1 className="text-center text-2xl">Hello hello! Welcome on board!</h1>
             <hr />
-            
-            
+
+
             <input
                 className="p-3 border border-green-500 rounded-lg mb-4 focus:outline-none focus:border-green-500"
                 id="username"
@@ -31,9 +31,9 @@ export default function SignupPage() {
                 placeholder="Type a username here.."
             />
 
-            
+
             <button onClick={onSignup} className="p-3 border border-green-500 rounded-lg mb-4 focus:outline-none focus:border-green-500">Save</button>
-            
+
 
         </div>
     )
