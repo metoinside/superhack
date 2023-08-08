a821a3fbebea912b50306ce0420565b0b90c7197
busra sener <b.sener1999@gmail.com>
2023-08-08 15:20:06 +0000

    color update

--- src/app/components/card.tsx
+++ src/app/components/card.tsx
@@ -6,7 +6,7 @@ export default function Card(props: {image: string, title: string, description: 
             <h2 className="card-title">{props.title}</h2>
             <p>{props.description}</p>
             <div className="card-actions justify-end">
-                <button className="btn btn-s text-white bg-blue-700 space-y-24">{props.button}</button>
+                <button className="btn btn-s text-white bg-accent space-y-24">{props.button}</button>
             </div>
         </div>
     </div>
