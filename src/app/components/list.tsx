4ce6d89a04fc202454daafc894e33c4188e980ba
busra sener <b.sener1999@gmail.com>
2023-08-08 15:54:58 +0000

    update

--- src/app/components/list.tsx
+++ src/app/components/list.tsx
@@ -6,11 +6,7 @@ export default function List() {
                 {/* head */}
                 <thead>
                     <tr>
-                        <th>
-                            <label>
-                                <input type="checkbox" className="checkbox" />
-                            </label>
-                        </th>
+                        <th></th>
                         <th className="text-base-200 text-xl">Event</th>
                         <th className="text-base-200 text-xl">Event Detail</th>
                         <th></th>
@@ -32,21 +28,19 @@ export default function List() {
                                     </div>
                                 </div>
                                 <div>
-                                    <div className="text-base-200 font-bold">Early childhood education</div>
-                                    <div className="text-base-200 opacity-50">Educate Org</div>
+                                    <div className="text-base-200/80 font-bold">Early childhood education</div>
+                                    <div className="text-base-200/50">Educate Org</div>
                                 </div>
                             </div>
                         </td>
-                        <td className="text-base-200">
+                        <td className="text-base-200/80">
                             Quality pre-primary education is the foundation of a child’s journey: Every stage of education that follows relies on its success.
                             <br />
-                            <span className="badge badge-ghost badge-sm">Buy a ticket for $5</span>
+                            <span className="badge badge-ghost badge-sm bg-accent border-accent">Buy a ticket for $5</span>
                         </td>
 
                         <th>
-                            <button className="btn btn-xs text-blue-900 bg-white space-y-24">details</button>
-
-                            <button className="btn btn-xs text-white bg-blue-700 space-y-24">donate</button>
+                            <button className="btn btn-xs bg-accent border-accent text-base-200 space-y-24">donate</button>
                         </th>
                     </tr>
                     {/* row 2 */}
@@ -64,21 +58,19 @@ export default function List() {
                                     </div>
                                 </div>
                                 <div>
-                                    <div className="text-base-200 font-bold">Gender Equality</div>
-                                    <div className="text-base-200 opacity-50">Equality org</div>
+                                    <div className="text-base-200/80 font-bold">Gender Equality</div>
+                                    <div className="text-base-200/50">Equality org</div>
                                 </div>
                             </div>
                         </td>
-                        <td className="text-base-200">
+                        <td className="text-base-200/80">
                             Aims all over the world to empower girls and women, and to ensure their full participation in political, social, and economic systems.
                             <br />
-                            <span className="badge badge-ghost badge-sm">Buy a ticket for $5</span>
+                            <span className="badge badge-ghost badge-sm bg-accent border-accent">Buy a ticket for $5</span>
                         </td>
 
                         <th>
-                            <button className="btn btn-xs text-blue-900 bg-white space-y-24">details</button>
-
-                            <button className="btn btn-xs text-white bg-blue-700 space-y-24">donate</button>
+                            <button className="btn btn-xs bg-accent border-accent text-base-200 space-y-24">donate</button>
                         </th>
                     </tr>
                     {/* row 3 */}
@@ -96,21 +88,19 @@ export default function List() {
                                     </div>
                                 </div>
                                 <div>
-                                    <div className="text-base-200 font-bold">Disabilities Association</div>
-                                    <div className="text-base-200 opacity-50">Time to break obstacles</div>
+                                    <div className="text-base-200/80 font-bold">Disabilities Association</div>
+                                    <div className="text-base-200/50">Time to break obstacles</div>
                                 </div>
                             </div>
                         </td>
-                        <td className="text-base-200">
+                        <td className="text-base-200/80">
                             Fifteen per cent of the world’s population – at least one billion people – have some form of disability, whether present at birth or acquired later in life. Nearly 240 million of them are children.
                             <br />
-                            <span className="badge badge-ghost badge-sm">Buy a ticket for $5</span>
+                            <span className="badge badge-ghost badge-sm bg-accent border-accent">Buy a ticket for $5</span>
                         </td>
 
                         <th>
-                            <button className="btn btn-xs text-blue-900 bg-white space-y-24">details</button>
-
-                            <button className="btn btn-xs text-white bg-blue-700 space-y-24">donate</button>
+                            <button className="btn btn-xs bg-accent border-accent text-base-200 space-y-24">donate</button>
                         </th>
                     </tr>
                     {/* row 4 */}
@@ -128,20 +118,19 @@ export default function List() {
                                     </div>
                                 </div>
                                 <div>
-                                    <div className="text-base-200 font-bold">Library and intelligence classes</div>
-                                    <div className="text-base-200 opacity-50">Educate Org</div>
+                                    <div className="text-base-200/80 font-bold">Library and intelligence classes</div>
+                                    <div className="text-base-200/50">Educate Org</div>
                                 </div>
                             </div>
                         </td>
-                        <td className="text-base-200">
-                            Socialization is a lifelong process that starts from the birth. In this process, school and primarily the family play an important role. Playing mind games and reading book are among key issues that influence positively the development of intelligence, learning new things and getting socialize in terms of children.          <br />
-                            <span className="badge badge-ghost badge-sm">Buy a ticket for $5</span>
+                        <td className="text-base-200/80">
+                            Socialization is a lifelong process that starts from the birth. In this process, school and primarily the family play an important role. Playing mind games and reading book are among key issues that influence positively the development of intelligence, learning new things and getting socialize in terms of children.          
+                            <br />
+                            <span className="badge badge-ghost badge-sm bg-accent border-accent">Buy a ticket for $5</span>
                         </td>
 
                         <th>
-                            <button className="btn btn-xs text-blue-900 bg-white space-y-24">details</button>
-
-                            <button className="btn btn-xs text-white bg-blue-700 space-y-24">donate</button>
+                            <button className="btn btn-xs bg-accent border-accent text-base-200 space-y-24">donate</button>
                         </th>
                     </tr>
                 </tbody>
