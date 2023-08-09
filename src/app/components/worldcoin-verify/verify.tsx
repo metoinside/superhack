af0e8105ff697baafbf97560022407663c9b678f
Metin Akın <aknmetin@gmail.com>
2023-08-09 15:39:21 +0300

    Type fix

--- src/app/components/worldcoin-verify/verify.tsx
+++ src/app/components/worldcoin-verify/verify.tsx
@@ -16,7 +16,7 @@ export default function VerifyID() {
             action="vote_1" // this is your action name from the Developer Portal
             onSuccess={onSuccess} // callback when the modal is closed
             handleVerify={handleVerify} // optional callback when the proof is received
-            credential_types={['orb']} // optional, defaults to ['orb']
+            credential_types={[CredentialType.Orb]} // optional, defaults to ['orb']
             enableTelemetry // optional, defaults to false
         >
             {({ open }) => <button onClick={open}>Verify with World ID</button>}
